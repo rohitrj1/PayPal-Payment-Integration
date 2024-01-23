6a406939f6fa682e7f862e57731318ae7b6798e9
rohit kumar <rraj70775@gmail.com>
2024-01-23 22:47:09 +0530

    Paypal payment Integration using spring boot

---
+++ PayalDemo/src/main/java/com/paypaldemo/PayalDemoApplication.java
@@ -0,0 +1,13 @@
+package com.paypaldemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PayalDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PayalDemoApplication.class, args);
+	}
+
+}
